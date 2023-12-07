095831703f7713fc2beaf1764924d8f0d1218d5d
fadillaoegi <fadilfdl2710@gmail.com>
2023-12-07 12:54:14 +0700

    commit first

---
+++ android/app/src/main/kotlin/com/example/uicloneinsta/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uicloneinsta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/routes/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static String root = "/uicloneinsta";
+  static String home = "$root/home";
+  static String myprofile = "$root/myprofile";
+  static String profile = "$root/profile";
+}
